943946a8a785ffa91c3bc6a04321045ec4ac5865
adammccollum@me.com <adammccollum@me.com>
2022-11-11 07:50:57 +0000

    Added Include to db context to retrieve nested sets

--- MinimalWorkoutApi/Program.cs
+++ MinimalWorkoutApi/Program.cs
@@ -19,11 +19,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/workoutEntries", async (WorkoutDbContext db) => await db.WorkoutEntries.ToListAsync()).WithOpenApi();
+app.MapGet("/workoutEntries", async (WorkoutDbContext db) => await db.WorkoutEntries.Include(a => a.Sets).ToListAsync()).WithOpenApi();
 
 app.MapGet("/workoutEntries/{id}", async (int id, WorkoutDbContext db) => 
 {
-    var workoutEntry = await db.WorkoutEntries.FindAsync(id);
+    var workoutEntry = await db.WorkoutEntries.Include(a => a.Sets).FindAsync(id);
 
     if(workoutEntry is null)
     {
@@ -45,7 +45,7 @@ app.MapPost("/workoutEntries", async (WorkoutEntry workout, WorkoutDbContext db)
 
 app.MapPut("/workoutEntries", async (int id, WorkoutEntry workout, WorkoutDbContext db) =>
 {
-    var workoutEntry = await db.WorkoutEntries.FindAsync(id);
+    var workoutEntry = await db.WorkoutEntries.Include(a => a.Sets).FindAsync(id);
 
     if (workoutEntry is null)
     {
