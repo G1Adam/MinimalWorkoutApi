b8920c63a62a56fb5d6cc773b859cfab4a732b42
adammccollum@me.com <adammccollum@me.com>
2022-11-30 08:21:25 +0000

    Adding in Workout and Set Validators. Tests to follow

---
+++ MinimalWorkoutApi/Validators/SetValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace MinimalWorkoutApi.Validators
+{
+    using FluentValidation;
+    using MinimalWorkoutApi.Models;
+
+    public class SetValidator : AbstractValidator<Set>
+    {
+        public SetValidator() 
+        { 
+            RuleFor(set => set.Repetitions).NotEmpty();
+            RuleFor(set => set.Weight).NotEmpty();
+            RuleFor(set => set.ExerciseName).NotNull().MaximumLength(150);
+        }
+    }
+}
---
+++ MinimalWorkoutApi/Validators/WorkoutEntryValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace MinimalWorkoutApi.Validators
+{
+    using FluentValidation;
+    using MinimalWorkoutApi.Models;
+
+    public class WorkoutEntryValidator : AbstractValidator<WorkoutEntry>
+    {
+        public WorkoutEntryValidator() 
+        {
+            RuleFor(wo => wo.Name).NotNull().MaximumLength(300);
+            RuleFor(wo => wo.WorkoutDate).NotEmpty();
+        }
+    }
+}
