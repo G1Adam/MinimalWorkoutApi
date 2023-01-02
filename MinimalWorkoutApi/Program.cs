107b078ad7ffdeb4d4e870645dc44de1578feb90
adammccollum@me.com <adammccollum@me.com>
2023-01-02 10:14:26 +0000

    Adding Serilog Boostrapping and RequestLogging Middleware

--- MinimalWorkoutApi/Program.cs
+++ MinimalWorkoutApi/Program.cs
@@ -2,28 +2,56 @@ using Microsoft.EntityFrameworkCore;
 using MinimalWorkoutApi.DatabaseContext;
 using MinimalWorkoutApi.Endpoints;
 using MinimalWorkoutApi.Extensions;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<WorkoutDbContext>(opt => opt.UseInMemoryDatabase("WorkoutEntries"));
-builder.Services.RegisterEndpointServices();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<WorkoutDbContext>(opt => opt.UseInMemoryDatabase("WorkoutEntries"));
+    builder.Services.RegisterEndpointServices();
+
+    builder.Host.UseSerilog((hostContext, services, configuration) =>
+    {
+        configuration
+            .WriteTo.File("minimalWorkoutApi-logs.txt")
+            .WriteTo.Console();
+    });
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.MapGroup("/workoutEntry").MapWorkoutEndpoints().WithOpenApi();
+
+    app.MapGroup("/workoutEntry/{id}/set").MapSetEndpoints().WithOpenApi();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();   
 }
 
-app.MapGroup("/workoutEntry").MapWorkoutEndpoints().WithOpenApi();
-
-app.MapGroup("/workoutEntry/{id}/set").MapSetEndpoints().WithOpenApi();
-
-app.Run();
 
